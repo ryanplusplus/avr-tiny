408234606193c2929bc7cb5bf18bcfbaf36aaa87
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-27 14:16:17 -0500

    Updated tiny; updated tools

--- src/hardware/pb5_heartbeat.c
+++ src/hardware/pb5_heartbeat.c
@@ -16,14 +16,13 @@ static tiny_timer_t timer;
 
 static void blink(tiny_timer_group_t* group, void* context)
 {
+  (void)group;
   (void)context;
   PINB |= pin_5;
-  tiny_timer_start(group, &timer, half_period_in_msec, blink, NULL);
 }
 
 void pb5_heartbeat_init(tiny_timer_group_t* timer_group)
 {
   DDRB |= pin_5;
-
-  blink(timer_group, NULL);
+  tiny_timer_start_periodic(timer_group, &timer, half_period_in_msec, blink, NULL);
 }
