72b3974862cb8d97de7f48a764d0c51514a9178c
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-27 22:12:59 -0500

    wtfffff

--- src/main.c
+++ src/main.c
@@ -5,34 +5,14 @@
 
 #include <stddef.h>
 #include <avr/interrupt.h>
-#include "timer0_system_tick.h"
-#include "pb5_heartbeat.h"
-#include "tiny_timer.h"
-#include "watchdog.h"
-
-static tiny_timer_group_t timer_group;
-static tiny_timer_t timer;
-
-static void kick_watchdog(tiny_timer_group_t* timer_group, void* context)
-{
-  (void)context;
-  watchdog_kick();
-  tiny_timer_start(timer_group, &timer, 50, kick_watchdog, NULL);
-}
 
 int main(void)
 {
   cli();
   {
-    watchdog_init(WDTO_250MS);
-    tiny_timer_group_init(&timer_group, timer0_system_tick_init());
-    pb5_heartbeat_init(&timer_group);
   }
   sei();
 
-  kick_watchdog(&timer_group, NULL);
-
-  while(true) {
-    tiny_timer_group_run(&timer_group);
+  while(1) {
   }
 }
--- target.mk
+++ target.mk
@@ -1,19 +1,19 @@
 TARGET := tiny
 BUILD_DIR := ./build
 
-MCU := atmega328p
+MCU := attiny85
 
 DEVICE_PACK := Atmel.ATmega_DFP.1.6.364
 
 # dwdebug or avrdude
-UPLOAD_TYPE := dwdebug
-AVRDUDE_PROGRAMMER_TYPE := dragon_isp
+UPLOAD_TYPE := avrdude
+AVRDUDE_PROGRAMMER_TYPE := usbtiny
 AVRDUDE_PROGRAMMER_ARGS :=
 DWDEBUG_TOOL := ttyUSB0
 
 FUSES := \
-  efuse=0xFD \
-  hfuse=0x9E \
+  efuse=0xFF \
+  hfuse=0x9D \
   lfuse=0xE2 \
 
 DEFINES := \
@@ -29,7 +29,6 @@ SRC_FILES := \
 
 SRC_DIRS := \
   src \
-  src/hardware \
 
 include lib_tiny.mk
 
