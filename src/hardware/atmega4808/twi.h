2395dabc084bc0e4976259bd8adb057c8dfbed7a
Ryan Hartlage <ryanplusplus@gmail.com>
2021-03-20 17:25:04 -0400

    Helpful links

---
+++ src/hardware/atmega4808/twi.h
@@ -0,0 +1,7 @@
+// Smart mode looks like it could be nice
+
+// one of these may be helpful:
+// https://github.com/arduino/ArduinoCore-megaavr/blob/master/libraries/Wire/src/utility/twi.c
+// https://www.avrfreaks.net/comment/2663266#comment-2663266
+// https://www.avrfreaks.net/comment/2672931#comment-2672931
+// https://www.avrfreaks.net/comment/2923141#comment-2923141
