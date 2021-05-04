6ccdf340b7616a86b32d8379a8ec68bec05f573e
Ryan Hartlage <ryanplusplus@gmail.com>
2021-05-03 20:24:49 -0400

    Updated libs

--- lib_tiny_cpp.mk
+++ lib_tiny_cpp.mk
@@ -19,7 +19,7 @@ tiny_cpp_INC_DIRS := \
 tiny_cpp_SYS_INC_DIRS := \
   lib/cpp/include \
 
-tiny_cpp_LIB_FILES := \
+tiny_cpp_SRC_FILES := \
 
-tiny_cpp_LIB_DIRS := \
+tiny_cpp_SRC_DIRS := \
   lib/tiny_cpp/src \
