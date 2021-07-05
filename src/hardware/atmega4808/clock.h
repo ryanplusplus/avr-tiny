10929cbf25501eccce2ecdedd303b01d1e79483e
Ryan Hartlage <ryanplusplus@gmail.com>
2021-07-05 14:28:59 -0400

    Replaced incorrect headers

--- src/hardware/atmega4808/clock.h
+++ src/hardware/atmega4808/clock.h
@@ -1,12 +1,10 @@
 /*!
  * @file
  * @brief
- *
- * Copyright GE Appliances - Confidential - All rights reserved.
  */
 
-#ifndef CLOCK_H
-#define CLOCK_H
+#ifndef clock_h
+#define clock_h
 
 #include <avr/io.h>
 
