d50222d769b995e06cee90b5c267aea1a4a4993e
Ryan Hartlage <ryanplusplus@gmail.com>
2021-12-29 09:20:42 -0500

    Neopixel driver for 8 MHz

---
+++ src/hardware/atmega328p/pb1_neopixel.c
@@ -0,0 +1,7 @@
+/*!
+ * @file
+ * @brief
+ */
+
+#include "pb1_neopixel.h"
+#include "neopixel_c_template.h"
--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
  * @brief
  */
 
-#include <avr/interrupt.h>
+#include "interrupts.h"
 #include "system_tick.h"
 #include "heartbeat.h"
 #include "tiny_timer.h"
@@ -14,14 +14,14 @@ int main(void)
 {
   static tiny_timer_group_t timer_group;
 
-  cli();
+  interrupts_disable();
   {
     clock_init();
     tiny_timer_group_init(&timer_group, system_tick_init());
     watchdog_init(&timer_group);
     heartbeat_init(&timer_group);
   }
-  sei();
+  interrupts_enable();
 
   while(1) {
     tiny_timer_group_run(&timer_group);
