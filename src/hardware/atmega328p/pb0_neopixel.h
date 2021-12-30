2749c24c1b48e35c4439948df7922afc26bb4c57
Ryan Hartlage <ryanplusplus@gmail.com>
2021-12-30 16:17:43 -0500

    PB0 neopixel for ATtiny85

---
+++ src/hardware/atmega328p/pb0_neopixel.h
@@ -0,0 +1,17 @@
+/*!
+ * @file
+ * @brief
+ */
+
+#ifndef pb0_neopixel_h
+#define pb0_neopixel_h
+
+#define neopixel_api_name pb0_neopixel
+#define neopixel_byte_order neopixel_byte_order_grb
+
+#define neopixel_port B
+#define neopixel_pin 0
+
+#include "neopixel_h_template.h"
+
+#endif
